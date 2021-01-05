27076e605740df8ceff23bcbb55f7b027371269e
mycognosist <gnomad@cryptolab.net>
2021-01-05 09:40:35 +0000

    Set wlan0 mode to default

--- scripts/activate_client.sh
+++ scripts/activate_client.sh
@@ -9,3 +9,5 @@ echo "Starting wpa_supplicant"
 /usr/bin/systemctl start wpa_supplicant
 echo "Setting wlan0 interface up"
 /usr/sbin/ifup wlan0
+echo "Setting wlan0 to default mode"
+/bin/ip link set wlan0 mode default
