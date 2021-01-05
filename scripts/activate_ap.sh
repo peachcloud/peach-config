565c37b3533bc3f505b5e6d597c39f72b70f8967
mycognosist <gnomad@cryptolab.net>
2021-01-05 09:39:21 +0000

    Unmask hostapd service

--- scripts/activate_ap.sh
+++ scripts/activate_ap.sh
@@ -5,6 +5,8 @@ echo "Stopping wpa_supplicant"
 /usr/bin/systemctl stop wpa_supplicant
 echo "Setting wlan0 interface down"
 /usr/sbin/ifdown wlan0
+echo "Unmasking hostapd"
+/usr/bin/systemctl unmask hostapd
 echo "Starting hostapd"
 /usr/bin/systemctl start hostapd
 echo "Starting dnsmasq"
